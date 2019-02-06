f27431b541be87a755b5d97a7a62369c355b487d
Mitsuru Mutaguchi <akagane99@gmail.com>
2019-02-06 18:07:53 +0900

    test: phpdoc fix

--- Test/Case/VideoTestUtil.php
+++ Test/Case/VideoTestUtil.php
@@ -2,7 +2,6 @@
 /**
  * VideoTest Utility
  *
- * @author Noriko Arai <arai@nii.ac.jp>
  * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
  * @link http://www.netcommons.org NetCommons Project
  * @license http://www.netcommons.org/license.txt NetCommons License
@@ -15,7 +14,7 @@ App::uses('TemporaryFolder', 'Files.Utility');
  * VideoTest Utility
  *
  * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
- * @package NetCommons\Mails\Utility
+ * @package NetCommons\CleanUp\Test\Case
  */
 class VideoTestUtil {
 
